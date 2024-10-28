e96f8b1d1f5cd4cc2f050f0d58939b8f27089b49
LionelPrz <lionelarg02@hotmail.com>
2024-10-28 14:20:05 -0300

    gfhjnfghfgh

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const shisho = document.getElementById("form_p");
+const shisho = document.getElementById("#form_p");
 const toast  = document.querySelector(".toast");
 const closeIcon = document.querySelector(".close");
 const progress = document.querySelector(".progress");
@@ -7,6 +7,7 @@ let timer1, timer2;
 
 console.log(shisho);
 
-shisho.addEventListener('focus',(e)=>{
-    console.log(e.target);
+shisho.addEventListener('focus',variable=>{
+    variable  = shisho.value;
+    console.log(variable);
 })
