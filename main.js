6546bc83e4bb4a63f8af531afe739106d0f76052
LionelPrz <lionelarg02@hotmail.com>
2024-10-28 13:31:26 -0300

    prueba de elementos

--- main.js
+++ main.js
@@ -1,32 +1,35 @@
-const button = document.querySelector("button");
+// const button = document.querySelector("button");
+const shisho = document.querySelector(".form-p > input");
 const toast  = document.querySelector(".toast");
 const closeIcon = document.querySelector(".close");
 const progress = document.querySelector(".progress");
 
 let timer1, timer2;
 
-button.addEventListener("click", () => {
-    console.log('click 1')
-  toast.classList.add("active");
-  progress.classList.add("active");
+console.log(shisho);
 
-  timer1 = setTimeout(() => {
-    toast.classList.remove("active");
-  }, 5000); //1s = 1000 milliseconds
+// button.addEventListener("click", () => {
+//     console.log('click 1')
+//   toast.classList.add("active");
+//   progress.classList.add("active");
 
-  timer2 = setTimeout(() => {
-    progress.classList.remove("active");
-  }, 5300);
-});
+//   timer1 = setTimeout(() => {
+//     toast.classList.remove("active");
+//   }, 5000); //1s = 1000 milliseconds
 
-closeIcon.addEventListener("click", () => {
-    console.log('click para cerrar la alerta ya sea manual o automaticamente.')
-  toast.classList.remove("active");
+//   timer2 = setTimeout(() => {
+//     progress.classList.remove("active");
+//   }, 5000);
+// });
 
-  setTimeout(() => {
-    progress.classList.remove("active");
-  }, 300);
+// closeIcon.addEventListener("click", () => {
+//     console.log('click para cerrar la alerta ya sea manual o automaticamente.')
+//   toast.classList.remove("active");
 
-  clearTimeout(timer1);
-  clearTimeout(timer2);
-});
+//   setTimeout(() => {
+//     progress.classList.remove("active");
+//   }, 300);
+
+//   clearTimeout(timer1);
+//   clearTimeout(timer2);
+// });
