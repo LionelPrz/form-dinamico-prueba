f65a788cd96970ec0fae581c661340f8d3fa4cb9
LionelPrz <lionelarg02@hotmail.com>
2024-10-28 14:34:18 -0300

    asdasdas

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const shisho = document.querySelector("#form_p");
+const shisho = document.querySelectorAll("input[id]");
 const toast  = document.querySelector(".toast");
 const closeIcon = document.querySelector(".close");
 const progress = document.querySelector(".progress");
@@ -7,7 +7,34 @@ let timer1, timer2;
 
 console.log(shisho);
 
-shisho.addEventListener('focus',variable=>{
-    variable  = shisho.value;
-    console.log(variable);
-})
+// const button = document.querySelector("button");
+
+// button.addEventListener("click", () => {
+//     console.log('click 1')
+//   toast.classList.add("active");
+//   progress.classList.add("active");
+
+//   timer1 = setTimeout(() => {
+//     toast.classList.remove("active");
+//   }, 5000); //1s = 1000 milliseconds
+
+//   timer2 = setTimeout(() => {
+//     progress.classList.remove("active");
+//   }, 5000);
+// });
+
+// closeIcon.addEventListener("click", () => {
+//     console.log('click para cerrar la alerta ya sea manual o automaticamente.')
+//   toast.classList.remove("active");
+
+//   setTimeout(() => {
+//     progress.classList.remove("active");
+//   }, 300);
+
+//   clearTimeout(timer1);
+//   clearTimeout(timer2);
+// });
+
+// function instructivo(){
+    
+// }
