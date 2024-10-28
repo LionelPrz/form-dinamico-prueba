5f715746b41f7a150bf89a3887d0e9fd7030476b
LionelPrz <lionelarg02@hotmail.com>
2024-10-28 13:39:28 -0300

    ahora si creo

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const shisho = document.querySelector(".form-p");
+const shisho = document.querySelector(".form_p > input");
 const toast  = document.querySelector(".toast");
 const closeIcon = document.querySelector(".close");
 const progress = document.querySelector(".progress");
