c0fae2a3d8b91034f4074790167c113899d354c2
LionelPrz <lionelarg02@hotmail.com>
2024-10-30 13:06:26 -0300

    Maquetacion base del formulario

--- app.js
+++ app.js
@@ -1,27 +1,49 @@
-const shisho = document.querySelectorAll("input[id]");
+// const shisho = document.querySelectorAll("input[id],select[id]");
 const toast  = document.querySelector(".toast");
 const closeIcon = document.querySelector(".close");
 const progress = document.querySelector(".progress");
+const form = document.querySelector('#form_p');
 
 let timer1, timer2;
 
-console.log(shisho);
+form.addEventListener('click',(e)=>{
+    console.log(e.target);
+    instructivo();
+});
 
-// const button = document.querySelector("button");
 
-// button.addEventListener("click", () => {
-//     console.log('click 1')
-//   toast.classList.add("active");
-//   progress.classList.add("active");
+function instructivo(){
+        toast.classList.add("active");
+        progress.classList.add("active");
+        timer1 = setTimeout(() => {
+            toast.classList.remove("active");
+          }, 5000); //1s = 1000 milliseconds
+        
+          timer2 = setTimeout(() => {
+            progress.classList.remove("active");
+          }, 5000);
+    }
 
-//   timer1 = setTimeout(() => {
-//     toast.classList.remove("active");
-//   }, 5000); //1s = 1000 milliseconds
-
-//   timer2 = setTimeout(() => {
-//     progress.classList.remove("active");
-//   }, 5000);
-// });
+//     toast.classList.add("active");
+    // shisho.addEventListener("focus", () => {
+    //     console.log('click 1')
+    //   toast.classList.add("active");
+    //   progress.classList.add("active");
+    
+    
+    // closeIcon.addEventListener("click", () => {
+    //     console.log('click para cerrar la alerta ya sea manual o automaticamente.')
+    //   toast.classList.remove("active");
+    
+    //   setTimeout(() => {
+    //     progress.classList.remove("active");
+    //   }, 300);
+    
+    //   clearTimeout(timer1);
+    //   clearTimeout(timer2);
+    // });
+    
+// }
 
 // closeIcon.addEventListener("click", () => {
 //     console.log('click para cerrar la alerta ya sea manual o automaticamente.')
@@ -34,7 +56,3 @@ console.log(shisho);
 //   clearTimeout(timer1);
 //   clearTimeout(timer2);
 // });
-
-// function instructivo(){
-    
-// }
